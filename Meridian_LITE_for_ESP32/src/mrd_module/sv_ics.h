467580ff6494f0546fac4fd478d9f3c270479187
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2024-08-19 22:45:02 +0900

    Add files via upload

--- Meridian_LITE_for_ESP32/src/mrd_module/sv_ics.h
+++ Meridian_LITE_for_ESP32/src/mrd_module/sv_ics.h
@@ -81,28 +81,27 @@ void mrd_sv_drive_ics_double(Meridim90Union &a_meridim, ServoParam &a_sv) {
 /// @param a_err_cnt サーボのエラーカウント
 /// @param a_stat サーボのステータス
 /// @param ics サーボクラスのインスタンス
-float mrd_servo_process_ics_gs2d(int a_servo_id, int a_cmnd, float a_tgt, float a_tgt_past, int a_trim,
-                            int a_cw, int &a_err_cnt, uint16_t &a_stat, IcsHardSerialClass &ics) {
-  int val_tmp = 0;
-  if (a_cmnd == 1) { // コマンドが1ならPos指定
-    val_tmp = ics.setPos(a_servo_id, mrd.Deg2Krs(a_tgt, a_trim, a_cw));
-  } else { // コマンドが0等なら脱力して値を取得
-    val_tmp = ics.setFree(a_servo_id);
-  }
+// float mrd_servo_process_ics_gs2d(int a_servo_id, int a_cmnd, float a_tgt, float a_tgt_past, int a_trim,
+//                             int a_cw, int &a_err_cnt, uint16_t &a_stat, IcsHardSerialClass &ics) {
+//   int val_tmp = 0;
+//   if (a_cmnd == 1) { // コマンドが1ならPos指定
+//     val_tmp = ics.setPos(a_servo_id, mrd.Deg2Krs(a_tgt, a_trim, a_cw));
+//   } else { // コマンドが0等なら脱力して値を取得
+//     val_tmp = ics.setFree(a_servo_id);
+//   }
+//   if (val_tmp == -1) { // サーボからの返信信号を受け取れなかった場合
+//     val_tmp = mrd.Deg2Krs(a_tgt_past, a_trim, a_cw);
+//     a_err_cnt++;
+//     if (a_err_cnt >= SERVO_LOST_ERR_WAIT) { // 一定以上の連続エラーで通信不能とみなす
+//       a_err_cnt = SERVO_LOST_ERR_WAIT;
+//       a_stat = 1;
+//     }
+//   } else {
+//     a_err_cnt = 0;
+//     a_stat = 0;
+//   }
 
-  if (val_tmp == -1) { // サーボからの返信信号を受け取れなかった場合
-    val_tmp = mrd.Deg2Krs(a_tgt_past, a_trim, a_cw);
-    a_err_cnt++;
-    if (a_err_cnt >= SERVO_LOST_ERR_WAIT) { // 一定以上の連続エラーで通信不能とみなす
-      a_err_cnt = SERVO_LOST_ERR_WAIT;
-      a_stat = 1;
-    }
-  } else {
-    a_err_cnt = 0;
-    a_stat = 0;
-  }
-
-  return mrd.Krs2Deg(val_tmp, a_trim, a_cw);
-}
+//   return mrd.Krs2Deg(val_tmp, a_trim, a_cw);
+// }
 
 #endif // __MERIDIAN_SERVO_KONDO_ICS_H__
