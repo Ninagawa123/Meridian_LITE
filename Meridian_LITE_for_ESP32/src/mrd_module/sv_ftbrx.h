4d09239b13071355ae4973e7c34c2c14d0914b1d
TenrouHoshina <masiro.to.akari@gmail.com>
2026-04-30 01:52:06 +0900

    mrd_moduleの更新

--- Meridian_LITE_for_ESP32/src/mrd_module/sv_ftbrx.h
+++ Meridian_LITE_for_ESP32/src/mrd_module/sv_ftbrx.h
@@ -1,15 +1,13 @@
 #ifndef __MERIDIAN_SERVO_FUTABA_RSxTTL_H__
 #define __MERIDIAN_SERVO_FUTABA_RSxTTL_H__
 
-#include "mrd_common.h"
-
 //==================================================================================================
 //  FUTABA RSxTTLサーボ関連の処理  ------------------------------------------------------------------
 //==================================================================================================
 
 /// @brief スタブ関数.
 /// @return 常にfalseを返す.
-inline bool mrd_servo_ftbrs_x() {
+bool mrd_servo_ftbrs_x() {
   return false;
 }
 
