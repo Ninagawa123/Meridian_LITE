3a388f2e441bc7f8bcbb0a835f0def18f8df2a27
TenrouHoshina <masiro.to.akari@gmail.com>
2026-04-30 01:54:19 +0900

    networkの更新

--- Meridian_LITE_for_ESP32/src/mrd_wifi.cpp
+++ Meridian_LITE_for_ESP32/src/mrd_wifi.cpp
@@ -1,20 +1,27 @@
 // mrd_wifi.cpp
 // WiFi関連の関数実装
 
+// ヘッダファイルの読み込み
 #include "mrd_wifi.h"
+#include "keys.h"
+
+// ライブラリ導入
+
+//==================================================================================================
+// 共有リソース
+//==================================================================================================
+WiFiUDP m_udp; // wifi設定
 
 //==================================================================================================
 //  WiFi関数
 //==================================================================================================
 
 /// @brief WiFiを初期化する
-/// @param a_udp WiFiUDPインスタンス
 /// @param a_ssid WiFiアクセスポイントのSSID
 /// @param a_pass WiFiアクセスポイントのパスワード
 /// @param a_serial 出力シリアル
 /// @return 成功時はtrue, 失敗時はfalse
-bool mrd_wifi_init(WiFiUDP &a_udp, const char *a_ssid, const char *a_pass,
-                   HardwareSerial &a_serial) {
+bool mrd_wifi_init(const char *a_ssid, const char *a_pass, HardwareSerial &a_serial) {
   WiFi.disconnect(true, true); // 新規接続のためWiFi接続をリセット
   delay(100);
   WiFi.begin(a_ssid, a_pass); // WiFiに接続
@@ -31,18 +38,17 @@ bool mrd_wifi_init(WiFiUDP &a_udp, const char *a_ssid, const char *a_pass,
       return false;
     }
   }
-  a_udp.begin(UDP_RECV_PORT);
+  m_udp.begin(UDP_RECV_PORT);
   return true;
 }
 
 /// @brief UDP経由でデータを受信しMeridim配列に格納する
 /// @param a_meridim_bval バイト型のMeridim配列
 /// @param a_len バイト型Meridim配列の長さ
-/// @param a_udp 使用するWiFiUDPインスタンス
 /// @return 受信した場合はtrue, 受信しなかった場合はfalse
-bool mrd_wifi_udp_receive(byte *a_meridim_bval, int a_len, WiFiUDP &a_udp) {
-  if (a_udp.parsePacket() >= a_len) {  // 受信バッファにデータがあるか確認
-    a_udp.read(a_meridim_bval, a_len); // データを受信
+bool mrd_wifi_udp_receive(byte *a_meridim_bval, int a_len) {
+  if (m_udp.parsePacket() >= a_len) {  // 受信バッファにデータがあるか確認
+    m_udp.read(a_meridim_bval, a_len); // データを受信
     return true;
   }
   return false; // バッファにデータなし
@@ -51,20 +57,19 @@ bool mrd_wifi_udp_receive(byte *a_meridim_bval, int a_len, WiFiUDP &a_udp) {
 /// @brief Meridim配列データをUDP経由でWIFI_SEND_IP, UDP_SEND_PORTへ送信する
 /// @param a_meridim_bval バイト型のMeridim配列
 /// @param a_len バイト型Meridim配列の長さ
-/// @param a_udp 使用するWiFiUDPインスタンス
 /// @return 成功時はtrue, 失敗時はfalse
 /// 内部でWIFI_SEND_IP, UDP_SEND_PORTを使用
-bool mrd_wifi_udp_send(byte *a_meridim_bval, int a_len, WiFiUDP &a_udp) {
-  int result = a_udp.beginPacket(WIFI_SEND_IP, UDP_SEND_PORT); // UDPパケット開始
+bool mrd_wifi_udp_send(byte *a_meridim_bval, int a_len) {
+  int result = m_udp.beginPacket(WIFI_SEND_IP, UDP_SEND_PORT); // UDPパケット開始
   if (result == 0) {
     return false; // パケット開始失敗
   }
 
-  size_t bytes_written = a_udp.write(a_meridim_bval, a_len); // データ書き込み
+  size_t bytes_written = m_udp.write(a_meridim_bval, a_len); // データ書き込み
   if (bytes_written != a_len) {
     return false; // 書き込みサイズ不一致
   }
 
-  result = a_udp.endPacket(); // UDPパケット終了
+  result = m_udp.endPacket(); // UDPパケット終了
   return (result == 1);       // 成功時は1を返す
 }
--- Meridian_LITE_for_ESP32/src/mrd_wifi.h
+++ Meridian_LITE_for_ESP32/src/mrd_wifi.h
@@ -2,40 +2,34 @@
 #define __MERIDIAN_WIFI_H__
 
 // ヘッダファイルの読み込み
-#include "keys.h"
-#include "mrd_common.h"
 
 // ライブラリ導入
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-extern WiFiUDP udp; // WiFi設定
 
 //==================================================================================================
 //  WiFi関数宣言
 //==================================================================================================
 
 /// @brief WiFiを初期化する
-/// @param a_udp WiFiUDPインスタンス
 /// @param a_ssid WiFiアクセスポイントのSSID
 /// @param a_pass WiFiアクセスポイントのパスワード
 /// @param a_serial 出力シリアル
 /// @return 成功時はtrue, 失敗時はfalse
-bool mrd_wifi_init(WiFiUDP &a_udp, const char *a_ssid, const char *a_pass,
-                   HardwareSerial &a_serial);
+bool mrd_wifi_init(const char *a_ssid, const char *a_pass, HardwareSerial &a_serial);
 
 /// @brief UDP経由でデータを受信しMeridim配列に格納する
 /// @param a_meridim_bval バイト型のMeridim配列
 /// @param a_len バイト型Meridim配列の長さ
-/// @param a_udp 使用するWiFiUDPインスタンス
 /// @return 受信した場合はtrue, 受信しなかった場合はfalse
-bool mrd_wifi_udp_receive(byte *a_meridim_bval, int a_len, WiFiUDP &a_udp);
+bool mrd_wifi_udp_receive(byte *a_meridim_bval, int a_len);
 
 /// @brief Meridim配列データをUDP経由でWIFI_SEND_IP, UDP_SEND_PORTへ送信する
 /// @param a_meridim_bval バイト型のMeridim配列
 /// @param a_len バイト型Meridim配列の長さ
-/// @param a_udp 使用するWiFiUDPインスタンス
 /// @return 成功時はtrue, 失敗時はfalse
 /// 内部でWIFI_SEND_IP, UDP_SEND_PORTを使用
-bool mrd_wifi_udp_send(byte *a_meridim_bval, int a_len, WiFiUDP &a_udp);
+bool mrd_wifi_udp_send(byte *a_meridim_bval, int a_len);
 
 #endif // __MERIDIAN_WIFI_H__
