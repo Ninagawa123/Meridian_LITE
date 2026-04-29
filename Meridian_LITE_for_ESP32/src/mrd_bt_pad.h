75802decae7b311650238c350812bc13dd18174a
TenrouHoshina <masiro.to.akari@gmail.com>
2026-04-30 01:54:46 +0900

    mrd_bt_padの更新

--- Meridian_LITE_for_ESP32/src/mrd_bt_pad.h
+++ Meridian_LITE_for_ESP32/src/mrd_bt_pad.h
@@ -2,34 +2,11 @@
 #define __MERIDIAN_BT_PAD_H__
 
 // ヘッダファイルの読み込み
-#include "config.h"
-#include <Arduino.h>
+#include "mrd_common.h"
 
 // ライブラリ導入
 #include <ESP32Wiimote.h> // Wiiコントローラー
-extern ESP32Wiimote wiimote;
-
 #include <IcsHardSerialClass.h>
-extern PadUnion pad_array;
-extern SemaphoreHandle_t pad_mutex; // pad_arrayアクセス用Mutex
-
-// リモコンボタンデータ変換テーブル
-constexpr unsigned short PAD_TABLE_WIIMOTE_SOLO[16] = {
-    0x1000, 0x0080, 0x0000, 0x0010, 0x0200, 0x0400, 0x0100, 0x0800,
-    0x0000, 0x0000, 0x0000, 0x0000, 0x0008, 0x0001, 0x0002, 0x0004};
-constexpr unsigned short PAD_TABLE_WIIMOTE_ORIG[16] = {
-    0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x0000, 0x0000, 0x0000,
-    0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0000, 0x0000, 0x0080};
-constexpr unsigned short PAD_TABLE_KRC5FH_TO_COMMON[16] = { //
-    0, 64, 32, 128, 1, 4, 2, 8, 1024, 4096, 512, 2048, 16, 64, 32, 256};
-
-// KRC-5FH 十字キーコンボ検出用ボタンマスク
-constexpr uint16_t KRC_DPAD_LEFT_MASK = 0x000F;   // bit 0-3: 左十字キー (UP,DOWN,LEFT,RIGHT)
-constexpr uint16_t KRC_DPAD_RIGHT_MASK = 0x0170;  // bit 4,5,6,8: 右十字キーボタン (368)
-constexpr uint16_t KRC_CLEAR_DPAD_LEFT = 0xFFF0;  // bit 0-3 をクリア
-constexpr uint16_t KRC_CLEAR_DPAD_RIGHT = 0xFE8F; // bit 4,5,6,8 をクリア
-// ボタンテーブル変換から残った可能性のあるbit 1,2 をクリア
-constexpr uint16_t KRC_CLEAR_RESIDUAL_BITS = 0xFFF9; // 0b1111111111111001
 
 //==================================================================================================
 //  JOYPAD 関数宣言
@@ -77,13 +54,11 @@ uint64_t mrd_pad_read(PadType a_pad_type, uint64_t a_pad_data, IcsHardSerialClas
 /// @brief Bluetoothを設定し, Wiiコントローラー接続を開始する
 /// @param a_mount_pad 搭載パッドタイプ
 /// @param a_timeout 接続タイムアウト
-/// @param a_wiimote Wiimoteインスタンス
 /// @param a_led LEDピン
 /// @param a_serial 出力シリアル
 /// @return 成功時はtrue, タイムアウト時はfalse
 bool mrd_bt_settings(int a_mount_pad,
                      int a_timeout,
-                     ESP32Wiimote &a_wiimote,
                      int a_led,
                      HardwareSerial &a_serial);
 
